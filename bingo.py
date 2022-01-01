199866447e660bf63b314f5074ceccae58108cc5
tbarbosa <tiagomartinho.barbosa@gmail.com>
2022-01-01 20:45:58 +0000

    Resolved some dependencies.
    Still necessary to solve the mp3 player dependency

--- bingo.py
+++ bingo.py
@@ -1,19 +1,23 @@
 import random
 import os
 import time
+import pip
 
 from gtts import gTTS
 from termcolor import cprint
 from pyfiglet import figlet_format
 
+
 LANG = "pt"
+MP3_player = "mpv"
+
 
 def create_bingo(ran):
     sorteio = random.sample(range(1,ran+1), ran)
     return [str(i) for i in sorteio ]
 
 
-def print_num(num):
+def print_num(string):
     cprint(figlet_format(string, font='starwars'))
 
 
@@ -22,20 +26,23 @@ def say_numbers(string_list):
         print_num(string)
         obj = gTTS(string, lang=LANG, slow = True)
         obj.save("num.mp3")
-        os.system("mpv num.mp3")
+        #repeat twice with 4 second interval
+        os.system(f"{MP3_player} num.mp3")
         time.sleep(4)
-        os.system("mpv num.mp3")
+        os.system(f"{MP3_player} num.mp3")
+        time.sleep(2)
 
 
-def import_or_install(package):
+def setup(package):
     try:
         __import__(package)
-    except ImportError
-        pip.main(['install', package]
+    except ImportError:
+        pip.main(['install', package])
 
 
-sorteio=create_bingo(90)
-say_numbers(sorteio)
-obj2 = gTTS("Acabou pudins", lang=LANG, slow = True)
-obj2.save("num.mp3")
-os.system("mpv num.mp3")
+if __name__=="__main__":
+    for pkg in ["gtts", "cprint", "pyfiglet"]:
+        setup(pkg)
+    sorteio=create_bingo(90)
+    say_numbers(sorteio)
+    os.system("rm num.mp3")
